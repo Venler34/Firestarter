c3c067807dc8ab780f942f0fb93160fd0290b7de
Toren Long <venlercode@gmail.com>
2024-09-15 01:58:56 -0400

    Made content buttons more nicer

--- frontend/my-project/components/uploadbutton.tsx
+++ frontend/my-project/components/uploadbutton.tsx
@@ -33,9 +33,23 @@ export default function UploadButton() {
         }
     }
     return (
-        <form className="bg-[#F92581] rounded-md content-center w-3/12 p-8" encType="multipart/form-data" onSubmit={onSubmit}>
-            <input type="file" onChange={handleFileChange}/>
-            <input type="submit"/>
+        <form className="w-3/12" encType="multipart/form-data" onSubmit={onSubmit}>
+            <div className="bg-[#F92581] rounded-md">
+                <label className="w-max" htmlFor="fileInput">
+                    <svg className="block m-auto"xmlns="http://www.w3.org/2000/svg" width="91" height="77" viewBox="0 0 91 77" fill="none">
+                        <path opacity="0.5" d="M66.75 27.8571C75.9937 27.8997 81.0003 28.2441 84.2643 30.9705C88 34.0909 88 39.1106 88 49.15V52.7C88 62.743 88 67.7627 84.2643 70.8831C80.5328 74 74.519 74 62.5 74H28.5C16.481 74 10.4672 74 6.73575 70.8831C3 67.7591 3 62.743 3 52.7V49.15C3 39.1106 3 34.0909 6.73575 30.9705C9.99975 28.2441 15.0063 27.8997 24.25 27.8571" stroke="#F9E9E9" stroke-width="5" stroke-linecap="round"/>
+                        <path d="M45.5 49.15V3M45.5 3L58.25 15.425M45.5 3L32.75 15.425" stroke="#F9E9E9" strokeWidth="5" stroke-linecap="round" stroke-linejoin="round"/>
+                    </svg>
+                    <p className="text-xl text-center text-[#FFF]">Upload your Lecture</p>
+                </label>
+                <input id="fileInput" className="text-[#F92581] hidden"type="file" onChange={handleFileChange}/>
+            </div>
+            <div className="text-center">
+                {selectedFile?.name || "No file selected"}
+            </div>
+            <div className="my-4 flex justify-center">
+                <input className="bg-[#25F99D] py-2 px-8 text-[#FFF]"type="submit"/>
+            </div>
         </form>
     )
 }
